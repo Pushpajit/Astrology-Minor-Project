af26342a77d8348f35804cc8aa721e222d356968
Pushpajit Biswas <pushpajitnexus007@gmail.com>
2023-11-02 19:58:35 +0530

    Extra button deleted

--- src/components/ZodiacCard.jsx
+++ src/components/ZodiacCard.jsx
@@ -10,9 +10,7 @@ function ZodiacCard({ name, date, description }) {
 
     const [ flip, setFlip ] = useState("flip-card");
     
-    function handleSwap(e){
-        setFlip("flip");
-    }
+    
 
     return (
         // <div className="h-fit border border-slate-500 w-1/4 group rounded-lg bg-white p-4 m-4 shadow-lg transform hover:scale-105 hover:shadow-xl hover:cursor-pointer transition-transform transition-shadow">
@@ -21,13 +19,13 @@ function ZodiacCard({ name, date, description }) {
         //     <p className="text-gray-700 mt-2">{description}</p>
         // </div>
         <div className={`${flip} hover:cursor-pointer`} >
-            <div className="flip-card-inner">
-                <div class="flip-card-front ">
+            <div className="flip-card-inner" >
+                <div class="flip-card-front " onClick={() => setFlip("flip")}>
                     <h2 className="text-2xl font-semibold">{name}</h2>
                     <p className="text-gray-600">{date}</p>
                     <p className="text-gray-700 mt-2">{description}</p>
 
-                    <Button onClick={handleSwap} color='warning' size='small' variant='outlined' sx={{width: 20, marginX: "auto", marginTop: 5}}>click</Button>
+                    {/* <Button onClick={handleSwap} color='warning' size='small' variant='outlined' sx={{width: 20, marginX: "auto", marginTop: 5}}>click</Button> */}
                 </div>
 
                 <div className="flip-card-back flex flex-col justify-between">
